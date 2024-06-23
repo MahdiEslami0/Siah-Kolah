5a7c254751128a00e47555e8c291c58098ed2ec3
mahdi.es <mahdi.es.work@gmail.com>
2024-06-24 01:45:08 +0330

    fix ui bug

--- resources/views/web/default/list_pay/index.blade.php
+++ resources/views/web/default/list_pay/index.blade.php
@@ -117,21 +117,26 @@
                     <div>
                         <h2 class="section-title">یک پرتال پرداخت انتخاب کنید</h2>
                     </div>
-                    <div class="d-flex align-items-center" style="gap: 5px" onclick="show_select_pay()">
-                        <div>
-                            <a>
-                                پیش وارزی
-                            </a>
-                        </div>
-                        <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-arrow-left" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd"
-                                    d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8" />
-                            </svg>
-                        </div>
-                    </div>
+                    @if (count($products) == 1)
+                        @if (count($products) == 1)
+                            <div class="d-flex align-items-center" style="gap: 5px" onclick="show_select_pay()">
+                                <div>
+                                    <a>
+                                        پیش وارزی
+                                    </a>
+                                </div>
+                                <div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                        fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd"
+                                            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8" />
+                                    </svg>
+                                </div>
+                            </div>
+                        @endif
+                    @endif
                 </div>
+
                 @include('web.default.components.pay_cards')
                 <div class="d-flex align-items-center justify-content-between mt-45">
                     <span class="font-16 font-weight-500 text-gray">{{ trans('financial.total_amount') }}
